32390347014e44cc196ca6ea15016acc2c01198b
etudie <39594459+etudie@users.noreply.github.com>
2024-02-19 19:04:31 +0000

    Header component init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header/>
         <img src="woofer.svg" className="App-logo" alt="logo" />
         <p>
           Woofer
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+const Header = () => {
+    return (
+        <div className="Header-container">
+        <img src="woofer.svg" className="Header-logo" alt="woofer logo" />
+        <h1 className="Header-title">Woofer</h1>
+        </div>
+    )
+}
+
+export default Header;
