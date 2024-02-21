239880d2c1c1a8c652ba262d3b207b6ceae4390e
etudie <39594459+etudie@users.noreply.github.com>
2024-02-21 13:35:08 +0000

    Mobile styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,10 +2,16 @@ import './styles.css';
 
 const Header = () => {
     return (
+        <>
+        <a href="/">
         <div className="Header-container">
+        
         <img src="woofer.svg" className="Header-logo" alt="woofer logo" />
         <h1 className="Header-title">Woofer</h1>
+        
         </div>
+        </a>
+        </>
     )
 }
 
